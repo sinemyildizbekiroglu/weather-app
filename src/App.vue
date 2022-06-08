ec11ec0b21dc11af906bdf5cefcf9c1b36773c46
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-06-08 09:54:08 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "",
+    }
+  },
+}
+</script>
+
+<style></style>
