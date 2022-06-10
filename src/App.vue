4ae841bbd94476b03d88a4dcb56bf8134cfb840a
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-06-10 22:18:43 +0200

    Update `App.vue`

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,18 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Amsterdam, NL</div>
+          <div class="date">Friday 10 January 2022</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">15°C</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -76,4 +88,35 @@ main {
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25)
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+  
+}
+
 </style>
