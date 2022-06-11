e780b850544b6ce46da7d58195059c206b7efd86
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-06-11 20:52:30 +0200

    Add style to `App.vue`

--- src/App.vue
+++ src/App.vue
@@ -114,9 +114,22 @@ main {
   padding: 10px 25px;
   color: #FFF;
   font-size: 102px;
-  font-weight: 900;
-
+  font-weight: 900; 
   
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25)
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700; 
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 
 </style>
