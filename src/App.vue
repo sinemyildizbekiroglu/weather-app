85a28ebfb4c2fe86983eb3b8d0f871527cd01ca8
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-06-14 21:15:52 +0200

    Update `App.vue`

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,17 @@
         />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Amsterdam, NL</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
           <div class="date">Friday 10 January 2022</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">15°C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -41,7 +43,7 @@ export default {
     fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(
-          `${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
         )
           .then((res) => {
             return res.json()
