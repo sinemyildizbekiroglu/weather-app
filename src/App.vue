00cd30834c48116e95f0509403256c80b8d1d84d
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-06-14 21:49:26 +0200

    Add `dateBuilder` function

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
           <div class="location">
             {{ weather.name }}, {{ weather.sys.country }}
           </div>
-          <div class="date">Friday 10 January 2022</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
@@ -53,6 +53,37 @@ export default {
     },
     setResults(results) {
       this.weather = results
+    },
+    dateBuilder() {
+      let d = new Date()
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ]
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ]
+      let day = days[d.getDay()]
+      let date = d.getDate()
+      let month = months[d.getMonth()]
+      let year = d.getFullYear()
+      return `${day} ${date} ${month} ${year}`
     },
   },
 }
